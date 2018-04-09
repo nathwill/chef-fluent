4125b666a5c99e208768349e67e2df16e97f7185
Nathan Williams <nath.e.will@gmail.com>
2018-04-09 15:59:00 -0700

    cops

--- metadata.rb
+++ metadata.rb
@@ -7,14 +7,8 @@ long_description 'Installs/Configures fluentd-agent'
 version '0.1.0'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/fluentd-agent/issues'
+supports 'centos', '~> 7.0'
+supports 'ubuntu', '~> 16.04'
 
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/fluentd-agent'
+issues_url 'https://github.com/nathwill/chef-fluent/issues'
+source_url 'https://github.com/nathwill/chef-fluent'
