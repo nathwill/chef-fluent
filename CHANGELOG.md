32c8a2cc7e0e030afdf05fa15679790bd05e2f82
Nathan Williams <nath.e.will@gmail.com>
2018-04-09 16:55:12 -0700

    bump ver

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 1.0.0 / 2018-04-09
+
+* ready for use
+
 # 0.1.0 / 2018-04-09
 
 * initial release
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'nath.e.will@gmail.com'
 license 'Apache-2.0'
 description 'Installs/Configures fluentd-agent'
 long_description 'Installs/Configures fluentd-agent'
-version '0.1.0'
+version '1.0.0'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
 supports 'centos', '~> 7.0'
